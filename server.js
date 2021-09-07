76e3cfc7b717dd447521b2de9d34874eb8bdb591
Kelven Luiz <kelvenluiz@usp.br>
2021-09-07 13:35:38 -0300

    Added nunjucks

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const server = express()
+const recipes = require('./data')
+
+server.use(express.static('public'))
+server.use(express.static('assets'))
+
+server.set("view engine", "njk")
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res) {
+    return res.render("index")
+})
+
+server.get("/layout", function(req, res) {
+    return res.render("layout")
+})
+
+server.get("/receitas", function(req, res) {
+    return res.render("receitas")
+})
+
+server.get("/sobre", function(req, res) {
+    return res.render("sobre")
+})
+
+server.listen(5000, function() {
+    console.log("Server is running!")
+})
